3f5352d3a1771b50bb12ad63376b13275b4eddca
Bjrn Nordlund <bjornno@gmail.com>
2011-07-07 13:23:56 +0200

    initial javazone

--- src/main/java/eu/nets/javazone/PaymentRoute.java
+++ src/main/java/eu/nets/javazone/PaymentRoute.java
@@ -3,4 +3,13 @@ package eu.nets.javazone;
 import org.apache.camel.builder.RouteBuilder;
 
 public class PaymentRoute extends RouteBuilder {
+
+    public static final String ENDPOINT_CLEARING = "direct:clearing";
+    public static final String ENDPOINT_BALANCE = "direct:balance";
+    public static final String ENDPOINT_RECEIPT = "direct:receipt";
+
+    @Override
+    public void configure() throws Exception {
+
+    }
 }
--- src/test/java/eu/nets/javazone/AcceptanceTest.java
+++ src/test/java/eu/nets/javazone/AcceptanceTest.java
@@ -1,11 +1,68 @@
 package eu.nets.javazone;
 
-/**
- * Created by IntelliJ IDEA.
- * User: bjn
- * Date: 7/7/11
- * Time: 12:47 PM
- * To change this template use File | Settings | File Templates.
- */
-public class AcceptanceTest {
+import org.apache.camel.EndpointInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.CamelTestSupport;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class AcceptanceTest extends CamelTestSupport {
+
+    @EndpointInject(uri = "mock:clearing")
+    private MockEndpoint clearing;
+
+    @EndpointInject(uri = "mock:balance")
+    private MockEndpoint balance;
+
+    @EndpointInject(uri = "mock:receipt")
+    private MockEndpoint receipt;
+
+    @Test
+    public void testReceivePayments() throws Exception {
+        balance.expectedMessageCount(5);
+        clearing.expectedMessageCount(1);
+        receipt.expectedMessageCount(1);
+
+        template.sendBody("file:data/receive", createPaymentFile(5));
+
+        balance.assertIsSatisfied();
+        clearing.assertIsSatisfied();
+        receipt.assertIsSatisfied();
+    }
+
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        RouteBuilder rb = new PaymentRoute();
+        rb.interceptSendToEndpoint(PaymentRoute.ENDPOINT_CLEARING).to(clearing);
+        rb.interceptSendToEndpoint(PaymentRoute.ENDPOINT_BALANCE).to(balance);
+        rb.interceptSendToEndpoint(PaymentRoute.ENDPOINT_RECEIPT).to(receipt);
+        return rb;
+    }
+
+    private InputStream createPaymentFile(int numberOfTransactions) {
+        StringBuffer transactions = new StringBuffer();
+        for(int i = 0; i<numberOfTransactions; i++) {
+            transactions.append(generateCreditAccount(i) + ";" + generateDebetAccount(i) + ";" + generateAmount(i) + "\n");
+        }
+        return IOUtils.toInputStream(transactions);
+    }
+
+    private String generateAmount(int i) {
+        return ""+i;
+    }
+
+    private String generateDebetAccount(int i) {
+        long seed = 11111111111L+i;
+        return Long.toString(seed).substring(0,11);
+    }
+
+    private String generateCreditAccount(int i) {
+        long seed = 22222222222L+i;
+        return Long.toString(seed).substring(0,11);
+    }
+
 }
