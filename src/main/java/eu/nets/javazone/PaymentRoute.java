85de9f96515757c27f4e7e8461e8bb2d5088e6ba
Bjrn Nordlund <bjornno@gmail.com>
2011-07-07 13:20:59 +0200

    initial javazone route

---
+++ src/main/java/eu/nets/javazone/PaymentRoute.java
@@ -0,0 +1,6 @@
+package eu.nets.javazone;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class PaymentRoute extends RouteBuilder {
+}
---
+++ src/test/java/eu/nets/javazone/AcceptanceTest.java
@@ -0,0 +1,11 @@
+package eu.nets.javazone;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bjn
+ * Date: 7/7/11
+ * Time: 12:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AcceptanceTest {
+}
