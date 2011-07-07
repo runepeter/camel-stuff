e4a3424bc3b39471d72a6fcb347ec0fd9cc60d81
Bjrn Nordlund <bjornno@gmail.com>
2011-07-07 14:02:45 +0200

    løsning test 1

--- src/main/java/eu/nets/javazone/PaymentRoute.java
+++ src/main/java/eu/nets/javazone/PaymentRoute.java
@@ -11,7 +11,20 @@ public class PaymentRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        from(ENDPOINT_RECEIVE)
+                .to(ENDPOINT_RECEIPT)
+                .split(body(String.class).tokenize("\n"))
+                .to(ENDPOINT_BALANCE)
+                .filter(header("BALANCE_CHECK").isEqualTo("OK"))
+                .aggregate(constant("*")).groupExchanges().completionTimeout(200)
+                .to(ENDPOINT_CLEARING);
 
+
+        from(ENDPOINT_BALANCE).setHeader("BALANCE_CHECK").constant("OK").log("balance called");
+
+        from(ENDPOINT_RECEIPT).log("receipt called");
+
+        from(ENDPOINT_CLEARING).log("clearing called");
 
     }
 }
