79cf1eed4c123362d5594c35d8577ac78b589b44
Bjrn Nordlund <bjornno@gmail.com>
2011-07-08 12:53:30 +0200

    cleaned up mess

--- database.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-java -cp ~/.m2/repository/org/hsqldb/hsqldb-j5/2.0.0/hsqldb-j5-2.0.0.jar org.hsqldb.Server -database.0 file:db/ -dbname.0 camel
-
--- src/main/resources/sql/tables.sql
+++
@@ -1,7 +0,0 @@
-create table batch(
-  reference VARCHAR(255) NOT NULL,
-  batchkey  VARCHAR(255) NOT NULL,
-  status    INTEGER NOT NULL,
-  version   INTEGER NOT NULL
-);
-
