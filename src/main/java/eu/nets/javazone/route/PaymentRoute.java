8d131289ddcf4afca92eb756df98202da9b3be3e
Bjrn Nordlund <bjornno@gmail.com>
2011-08-22 17:57:50 +0200

    ryddet opp litt ubrukte klasser og ruter

--- src/main/java/eu/nets/javazone/route/PaymentRoute.java
+++ src/main/java/eu/nets/javazone/route/PaymentRoute.java
@@ -1,20 +1,15 @@
 package eu.nets.javazone.route;
 
 import eu.nets.javazone.service.BalanceValidator;
-import eu.nets.javazone.service.CSMInsert;
 import org.apache.camel.Exchange;
-import org.apache.camel.Expression;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
-import org.apache.camel.processor.aggregate.GroupedExchangeAggregationStrategy;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class PaymentRoute extends RouteBuilder {
@@ -38,12 +33,7 @@ public class PaymentRoute extends RouteBuilder {
 
         from(ENDPOINT_RECEIVE)
                 .routeId("receive")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        startTime.set(System.currentTimeMillis());
-                    }
-                })
+                .process(new StartTimingProcessor())
                 .transacted()
                 .inOnly(ENDPOINT_RECEIPT)
                 .split(body().tokenize("\n")).setHeader("JallaCorrelationId", constant("RUNE"))
@@ -52,12 +42,6 @@ public class PaymentRoute extends RouteBuilder {
         from(ENDPOINT_RECEIPT).routeId("receipt").log("receipt called");
 
         from(ENDPOINT_BALANCE + "?concurrentConsumers=100&maxConcurrentConsumers=100&transacted=true").transacted().routeId("balance")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.err.println(new Date() + " :: " + Thread.currentThread().getName());
-                    }
-                })
                 .delay(1500)
                 .setHeader("BALANCE_CHECK")
                 .constant("OK")
@@ -66,44 +50,26 @@ public class PaymentRoute extends RouteBuilder {
                 .validate(bean(BalanceValidator.class))
                 .to(ENDPOINT_CLEARING_AGGREGATOR + "?transferExchange=true");
 
+
         from(ENDPOINT_CLEARING_AGGREGATOR + "?concurrentConsumers=100&maxConcurrentConsumers=100&transacted=true").filter(header("BALANCE_CHECK").isEqualTo("OK"))
                 .aggregate(header("JallaCorrelationId"), groupExchanges())
                 .aggregationRepositoryRef("aggregatorRepository")
                 .completionSize(1000)
                 .completionTimeout(30000)
                 .discardOnCompletionTimeout()
-
-                .onCompletion().process(new Processor() {
-            @Override
-            public void process(Exchange exchange) throws Exception {
-
-                if (exchange.getProperty("CamelAggregatedCompletedBy").equals("timeout")) {
-                    System.err.println("TIMEOUT, BABY!!");
-                }
-
-            }
-        }).end()
-     //   .transform(property("CamelGroupedExchange"))
                 .to(ENDPOINT_CLEARING)
-                ;
+        ;
 
         from(ENDPOINT_CLEARING).routeId("clearing")
                 .beanRef("csminsert")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.err.println(exchange.getIn().getBody());
-                    }
-                })
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.err.println("Took " + (System.currentTimeMillis() - startTime.get()) / 1000 + " secs.");
-                    }
-                })
+                .process(new StopTimingProcessor())
                 .log("clearing called");//.beanRef("csminsert");
 
     }
+
+
+
+
 
 
     public static AggregationStrategy groupExchanges() {
@@ -132,10 +98,17 @@ public class PaymentRoute extends RouteBuilder {
         };
     }
 
-    private static void assertNoError(Exchange newExchange) {
-        if (newExchange.getIn().getBody(String.class).contains("ERROR")) {
-            throw new RuntimeException();
+    class StartTimingProcessor implements Processor {
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            startTime.set(System.currentTimeMillis());
         }
     }
 
+    class StopTimingProcessor implements Processor {
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            System.err.println("Last file processed in " + (System.currentTimeMillis() - startTime.get()) / 1000 + " secs in total.");
+        }
+    }
 }
