6a69a402e1825ee88a56d3e92e7d4898dcc163f1
Bjrn Nordlund <bjornno@gmail.com>
2011-09-06 13:15:49 +0200

    aggregator repo

--- src/main/java/eu/nets/javazone/route/PaymentRoute.java
+++ src/main/java/eu/nets/javazone/route/PaymentRoute.java
@@ -43,6 +43,7 @@ public class PaymentRoute extends RouteBuilder {
         from(CLEARING_AGGREGATOR)
                 .aggregate(header("MyCorrelationId"), groupExchanges())
                 .completionSize(1000)
+                .aggregationRepositoryRef("aggregatorRepository")
                 .to(CLEARING);
 
 
